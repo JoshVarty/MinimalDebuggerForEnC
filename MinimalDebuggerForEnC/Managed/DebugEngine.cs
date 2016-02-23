3c478a179bbd70ea5712b95958b0da3d447326cd
Josh Varty <joshvarty@gmail.com>
2016-02-23 03:03:33 -0500

    General structure

---
+++ MinimalDebuggerForEnC/Managed/DebugEngine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinimalDebuggerForEnC.Managed
+{
+    public class DebugEngine : MarshalByRefObject
+    {
+        private ProcessCollection _processManager;
+        public DebugEngine()
+        {
+            _processManager = new ProcessCollection(this);
+        }
+
+        public DebugProcess Attach(int processId, string version)
+        {
+            return Attach(processId, null, version);
+        }
+
+        public DebugProcess Attach(int processId, SafeWin32Handle handle, string version)
+        {
+            //var debugProcess = _processManager.CreateLocalProcess(new CLRDebugger(version));
+            return null;
+
+        }
+    }
+}
