c9b11b8d1a2ac58d9f4ffaf02ba1f5073a0c1cce
Josh Varty <joshvarty@gmail.com>
2016-02-25 01:33:10 -0500

    App small startup project

---
+++ SampleProcess/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleProcess
+{
+    /// <summary>
+    /// This is a little command line application that our debugger will start up so we can test things out on it.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
