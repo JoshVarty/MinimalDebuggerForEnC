41bd9b5204c5e455568c95338f7d54411fafebb0
Josh Varty <joshvarty@gmail.com>
2016-02-26 02:33:42 -0500

    Simplify program for easier proof of concept EnC

--- SampleProcess/Program.cs
+++ SampleProcess/Program.cs
@@ -1,33 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace SampleProcess
+class Program
 {
-    /// <summary>
-    /// This is a little command line application that our debugger will start up so we can test things out on it.
-    /// </summary>
-    class Program
+    static System.Timers.Timer timer = new System.Timers.Timer();
+    static int count = 0;
+    static void Main(string[] args)
     {
-        static System.Timers.Timer timer = new System.Timers.Timer();
-        static int count = 0;
-        static void Main(string[] args)
-        {
-            var processId = Process.GetCurrentProcess().Id;
-            Console.WriteLine("Process Id: " + processId);
+        timer.Elapsed += Timer_Elapsed;
+        timer.Interval = 2000;
+        timer.Start();
+        Console.ReadLine();
+    }
 
-            timer.Elapsed += Timer_Elapsed;
-            timer.Interval = 2000;
-            timer.Start();
-            Console.ReadLine();
-        }
-
-        private static void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
-        {
-            Console.WriteLine("Hit " + count++);
-        }
+    private static void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+    {
+        Console.WriteLine("Hit " + count++);
     }
 }
