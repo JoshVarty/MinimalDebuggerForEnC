39a3854d3989c34ead8ed68883efb73350fc9e12
Josh Varty <joshvarty@gmail.com>
2016-02-26 02:58:19 -0500

    Add comment, make types explicity

--- MinimalDebuggerForEnC/Program.cs
+++ MinimalDebuggerForEnC/Program.cs
@@ -44,21 +44,24 @@ namespace MinimalDebuggerForEnC
 
             //Same as above, if we create the process it's immediately suspended
             //var corProcess = debugger.CreateProcess("SampleProcess.exe", "", ".", 0x10);
-            var corProcess = debugger.DebugActiveProcess(process.Id, win32Attach: false);
+            CorProcess corProcess = debugger.DebugActiveProcess(process.Id, win32Attach: false);
 
-            var appDomains = MakeGeneric<CorAppDomain>(corProcess.AppDomains);
+            List<CorAppDomain> appDomains = MakeGeneric<CorAppDomain>(corProcess.AppDomains);
             var firstDomain = appDomains.Single();
-            var assemblies = MakeGeneric<CorAssembly>(firstDomain.Assemblies);
+            List<CorAssembly> assemblies = MakeGeneric<CorAssembly>(firstDomain.Assemblies);
 
-            var sampleProcessAssembly = assemblies.Where(n => n.Name.EndsWith("SampleProcess.exe")).Single();
-            var sampleProcessModule = MakeGeneric<CorModule>(sampleProcessAssembly.Modules).Single();
+            CorAssembly sampleProcessAssembly = assemblies.Where(n => n.Name.EndsWith("SampleProcess.exe")).Single();
+            CorModule sampleProcessModule = MakeGeneric<CorModule>(sampleProcessAssembly.Modules).Single();
 
             var metadataDelta = new byte[] { 66, 83, 74, 66, 1, 0, 1, 0, 0, 0, 0, 0, 12, 0, 0, 0, 118, 52, 46, 48, 46, 51, 48, 51, 49, 57, 0, 0, 0, 0, 6, 0, 124, 0, 0, 0, 96, 1, 0, 0, 35, 45, 0, 0, 220, 1, 0, 0, 148, 0, 0, 0, 35, 83, 116, 114, 105, 110, 103, 115, 0, 0, 0, 0, 112, 2, 0, 0, 24, 0, 0, 0, 35, 85, 83, 0, 136, 2, 0, 0, 48, 0, 0, 0, 35, 71, 85, 73, 68, 0, 0, 0, 184, 2, 0, 0, 28, 0, 0, 0, 35, 66, 108, 111, 98, 0, 0, 0, 212, 2, 0, 0, 0, 0, 0, 0, 35, 74, 84, 68, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 167, 1, 67, 4, 0, 192, 8, 0, 0, 0, 0, 250, 1, 51, 0, 22, 0, 0, 1, 0, 0, 0, 5, 0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 10, 0, 0, 0, 10, 0, 0, 0, 2, 0, 0, 0, 1, 0, 1, 2, 0, 0, 2, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 14, 0, 0, 0, 87, 2, 0, 0, 42, 2, 0, 0, 18, 0, 0, 0, 49, 2, 0, 0, 66, 2, 0, 0, 14, 0, 0, 0, 203, 1, 0, 0, 42, 2, 0, 0, 14, 0, 0, 0, 250, 1, 0, 0, 42, 2, 0, 0, 14, 0, 0, 0, 232, 1, 0, 0, 42, 2, 0, 0, 4, 0, 0, 0, 0, 0, 145, 0, 218, 1, 0, 0, 153, 0, 0, 0, 0, 0, 0, 0, 121, 0, 0, 0, 80, 2, 0, 0, 133, 0, 0, 0, 129, 0, 0, 0, 240, 1, 0, 0, 139, 0, 0, 0, 3, 0, 0, 35, 0, 0, 0, 0, 4, 0, 0, 35, 0, 0, 0, 0, 13, 0, 0, 10, 0, 0, 0, 0, 14, 0, 0, 10, 0, 0, 0, 0, 12, 0, 0, 1, 0, 0, 0, 0, 13, 0, 0, 1, 0, 0, 0, 0, 14, 0, 0, 1, 0, 0, 0, 0, 15, 0, 0, 1, 0, 0, 0, 0, 16, 0, 0, 1, 0, 0, 0, 0, 2, 0, 0, 6, 0, 0, 0, 0, 12, 0, 0, 1, 13, 0, 0, 1, 14, 0, 0, 1, 15, 0, 0, 1, 16, 0, 0, 1, 2, 0, 0, 6, 13, 0, 0, 10, 14, 0, 0, 10, 3, 0, 0, 35, 4, 0, 0, 35, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 144, 0, 0, 0, 209, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 144, 0, 0, 0, 42, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 73, 110, 116, 51, 50, 0, 109, 115, 99, 111, 114, 108, 105, 98, 0, 84, 105, 109, 101, 114, 95, 69, 108, 97, 112, 115, 101, 100, 0, 67, 111, 110, 115, 111, 108, 101, 0, 87, 114, 105, 116, 101, 76, 105, 110, 101, 0, 83, 116, 114, 105, 110, 103, 0, 48, 54, 102, 54, 98, 101, 102, 52, 45, 102, 97, 99, 50, 45, 52, 102, 102, 56, 45, 98, 55, 54, 52, 45, 52, 48, 53, 97, 54, 55, 97, 54, 50, 53, 51, 98, 46, 100, 108, 108, 0, 83, 121, 115, 116, 101, 109, 0, 69, 108, 97, 112, 115, 101, 100, 69, 118, 101, 110, 116, 65, 114, 103, 115, 0, 83, 121, 115, 116, 101, 109, 46, 84, 105, 109, 101, 114, 115, 0, 67, 111, 110, 99, 97, 116, 0, 79, 98, 106, 101, 99, 116, 0, 0, 19, 49, 0, 50, 0, 51, 0, 52, 0, 53, 0, 54, 0, 55, 0, 56, 0, 32, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 67, 250, 71, 45, 35, 181, 12, 72, 156, 161, 71, 254, 150, 164, 125, 26, 108, 9, 11, 233, 54, 105, 11, 71, 187, 6, 12, 184, 183, 58, 127, 78, 0, 5, 0, 2, 14, 28, 28, 4, 0, 1, 1, 14, 8, 183, 122, 92, 86, 25, 52, 224, 137, 6, 0, 2, 1, 28, 18, 53 };
             var ilDelta = new byte[] { 0, 0, 0, 0, 146, 0, 114, 25, 0, 0, 112, 126, 2, 0, 0, 4, 37, 23, 88, 128, 2, 0, 0, 4, 140, 14, 0, 0, 1, 40, 13, 0, 0, 10, 40, 14, 0, 0, 10, 0, 42 };
+
+            //Calls down to ICorDebugModule2::ApplyChanges
+            //I'm currently getting AccessViolationExceptions, so I'm assuming my IL is incorrect :(
             sampleProcessModule.ApplyChanges(metadataDelta, ilDelta);
         }
 
-        private static IEnumerable<T> MakeGeneric<T>(IEnumerable enumerable)
+        private static List<T> MakeGeneric<T>(IEnumerable enumerable)
         {
             var list = new List<T>();
 
