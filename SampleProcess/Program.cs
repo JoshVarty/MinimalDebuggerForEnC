d6894ae3c02adce5d7a291388b6847fc3c0a1784
Josh Varty <joshvarty@gmail.com>
2016-02-27 05:57:05 -0500

    Simplify, try using smaller code

--- SampleProcess/Program.cs
+++ SampleProcess/Program.cs
@@ -1,19 +1,5 @@
-﻿using System;
-
-class Program
+﻿public class C
 {
-    static System.Timers.Timer timer = new System.Timers.Timer();
-    static int count = 0;
-    static void Main(string[] args)
-    {
-        timer.Elapsed += Timer_Elapsed;
-        timer.Interval = 2000;
-        timer.Start();
-        Console.ReadLine();
-    }
-
-    private static void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
-    {
-        Console.WriteLine("ORIGINAL " + count++);
-    }
+    public static void Main() { F(); }
+    public static void F() { System.Console.WriteLine(1); System.Console.ReadLine(); }
 }
