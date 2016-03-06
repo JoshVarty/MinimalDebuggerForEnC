f476ee140e03fad8188fc508c4fb18ab4fb2bbc6
Josh Varty <joshvarty@gmail.com>
2016-03-05 20:10:26 -0500

    Update program we're working with.

--- SampleProcess/Program.cs
+++ SampleProcess/Program.cs
@@ -1,5 +1,5 @@
 ﻿public class C
 {
     public static void Main() { F(); }
-    public static void F() { System.Console.WriteLine(1); System.Console.ReadLine(); }
+    public static void F() { System.Console.WriteLine(1); System.Console.ReadLine(); System.Console.WriteLine(1); System.Console.ReadLine(); }
 }
